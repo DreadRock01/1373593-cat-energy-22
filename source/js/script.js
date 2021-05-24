0fc7ec259f94fc17e0909da7d2e4693b1c9305e3
DreadRock01 <dreadrock01@users.noreply.github.com>
2021-05-24 20:12:20 +0500

    Доработка сеток и встраивание адаптивной графики

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,7 @@ const navList = document.querySelector('.nav-list');
 const menuBtn = document.querySelector('.nav__toggle');
 
 navList.classList.add('nav-list--closed');
-menuBtn.classList.remove("nav__toggle--nojs");
+menuBtn.classList.remove('nav__toggle--nojs');
 
 menuBtn.addEventListener('click', () =>  {
   menuBtn.classList.toggle('nav__toggle--closed');
